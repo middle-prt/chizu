f9c89a12e037eeedee7a897951fb8a932e5fc59c
morizo <morizo@localhost.localdomain>
2019-06-07 09:06:58 +0900

    01:49

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1. gem install sinatra
 2. gem install sinatra-reloader
+3. ruby app.rb
+4. Access-to   http://localhost:4567/
--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
 get '/' do
-  'Hello world!'
+  erb:index
+end
+
+post '/' do
+  @main_cate = params[:main_cate]
+  @from_date = params[:from_date]
+  @to_date = params[:to_date]
+  erb:index
 end
